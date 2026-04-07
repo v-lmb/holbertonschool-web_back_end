f4c65cc0791b183a21f701704533653c2242af9e
v-lmb <virginie.amamelia@gmail.com>
2026-04-07 16:52:01 +0200

    Add: description

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-
+"""
+Summary
+"""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """
+    function sum_list
+    """
     result: float = 0
     for i in range(len(input_list)):
         result += input_list[i]
