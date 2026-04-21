3dcd7cbfecc5d16e7dc9fa07dcc8b2f9dbe2e382
v-lmb <virginie.amamelia@gmail.com>
2026-04-21 19:46:02 +0200

    Feat: Change school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Change school topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    function update_topics
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
