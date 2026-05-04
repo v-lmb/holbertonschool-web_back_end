8258f65328b60b30463f8a14f7a31a3a9dff01aa
v-lmb <virginie.amamelia@gmail.com>
2026-05-04 17:56:02 +0200

    Feat: Create a function named groceriesList that returns a map of groceries

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList(name, quantity) {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
