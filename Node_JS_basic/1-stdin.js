af6a546cd133fcb00cf7af8d27159cfac885df97
v-lmb <virginie.amamelia@gmail.com>
2026-05-11 16:27:32 +0200

    Fix: add setEncoding

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,5 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
