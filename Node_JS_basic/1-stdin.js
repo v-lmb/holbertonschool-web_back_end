74ce4a5f1e57f0664f8c8cafeffa57328b6de78c
v-lmb <virginie.amamelia@gmail.com>
2026-05-11 16:32:31 +0200

    Remove: \n & name variable

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${data.toString()}`);
 });
 
 process.stdin.on('end', () => {
